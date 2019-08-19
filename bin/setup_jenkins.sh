16c8797faec35a05f7f78b48b039f002104afbd4
Gabriela-Phillips <53588824+Gabriela-Phillips@users.noreply.github.com>
2019-08-19 00:16:15 -0400

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -48,7 +48,7 @@ items:
         name: "jenkins-agent-appdev:latest"
     source:
       dockerfile: |
-        FROM openshift/jenkins-agent-maven-35-centos7:v3.11/
+        FROM openshift/jenkins-agent-maven-35-centos7
         USER root
         RUN yum -y install skopeo apb && yum clean all
         USER 1001
